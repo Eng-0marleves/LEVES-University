d1ad23efd5758ba33f09b0b84b7498994e901223
omar <0marleves99@gmail.com>
2023-12-31 20:26:09 +0200

    Committing changes before update/installation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", redirectTo: 'login', pathMatch: "full" },
+	{ path: "login", component: LoginPageComponent },
+	{ path: "home", component: HomePageComponent },
+	{ path: "**", component: NotFoundPageComponent }
+];
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [CarouselComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+
+}
