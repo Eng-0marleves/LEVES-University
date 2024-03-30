bef8561e6c2fe6d098f8862151c2ab83b916358f
Ahmed Abd Elnaser <102987038+Eng-0marleves@users.noreply.github.com>
2024-03-30 10:03:25 +0200

    day

---
+++ src/components/library/BookCard.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="book-card">
+		<div class="book-cover">
+			<img :src="cover" :alt="title" />
+		</div>
+		<div class="book-info">
+			<h2 class="book-title">{{ title }}</h2>
+			<p class="book-author">{{ author }}</p>
+		</div>
+		<button class="btn"><router-link class="router" to="">View Book</router-link></button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BookCard',
+	props: {
+		title: String,
+		author: String,
+		cover: String,
+	},
+	data() {
+		return {
+			// Add local component data here if needed
+		};
+	},
+	methods: {
+		// Define component methods here if needed
+	},
+	computed: {
+		// Define computed properties here if needed
+	},
+};
+</script>
+
+<style scoped>
+.book-card {
+	display: flex;
+	flex-direction: column;
+	width: 200px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	overflow: hidden;
+	margin: 10px;
+	border-radius: 8px;
+}
+
+.book-cover img {
+	width: 100%;
+	height: auto;
+}
+
+.book-info {
+	padding: 8px;
+}
+
+.book-title {
+	font-size: 16px;
+	margin: 10px 0 5px 0;
+}
+
+.book-author {
+	font-size: 14px;
+	color: #666;
+	margin: 0;
+}
+
+.book-description {
+	font-size: 12px;
+	color: #888;
+	margin-top: 5px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 3;
+	/* Limit to 3 lines */
+	-webkit-box-orient: vertical;
+}
+
+.router {
+	color: var(--white-color);
+}
+
+.btn {
+	background: var(--primary-color);
+	width: fit-content;
+	margin-left: auto;
+	border-radius: 0px;
+}
+</style>
--- src/views/library/LibraryHome.vue
+++ src/views/library/LibraryHome.vue
@@ -1,11 +1,42 @@
 <template>
 	<div class="library-home">
 		<HomeCarousel />
+
+		<div class="new-books mt-5">
+			<h2>New Books</h2>
+			<div class="books-conatiner">
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+			</div>
+		</div>
+
+		<div class="all-books mt-5">
+			<h2>All Books</h2>
+			<div class="books-conatiner">
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+				<BookCard title="title" author="author" cover="https://i.huffpost.com/gen/1039678/original.jpg" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import HomeCarousel from '@/components/library/HomeCarousel.vue';
+import BookCard from '@/components/library/BookCard.vue';
 
 export default {
 	name: 'LibraryHome',
@@ -15,7 +46,8 @@ export default {
 		};
 	},
 	components: {
-		HomeCarousel
+		HomeCarousel,
+		BookCard
 	},
 	methods: {
 		// Your methods go here
@@ -30,5 +62,18 @@ export default {
 </script>
 
 <style scoped>
-/* Your styles go here */
+.library-home {
+	padding-bottom: 10px;
+}
+
+.books-conatiner {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-wrap: wrap;
+}
+
+h2 {
+	text-align: center;
+}
 </style>
